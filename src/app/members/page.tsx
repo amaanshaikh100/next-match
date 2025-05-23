725eeed8d6b6231b731f1bbfe7ec6ebcd516cbea
Amaan Shaikh <shaikhamaannn100@gmail.com>
2025-05-24 00:58:02 +0530

    Inital commit

---
+++ src/app/members/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function MembersPage() {
+  return (
+    <div>
+      <h3 className="text-3xl">This will be a memebers page</h3>
+      <Link href="/">Go to home</Link>
+    </div>
+  );
+}
