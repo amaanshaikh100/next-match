dd03c6912be338880827b7f6346ae4337a4edfe9
Amaan Shaikh <shaikhamaannn100@gmail.com>
2025-06-07 19:50:28 +0530

    added like model and added the like button to the members card

--- src/app/members/page.tsx
+++ src/app/members/page.tsx
@@ -1,14 +1,20 @@
+import { fetchCurrentUserLikeIds } from "../actions/likeActions";
 import { getMembers } from "../actions/memberActions";
 import MemberCard from "./MemberCard";
 
 export default async function MembersPage() {
   const members = await getMembers();
+  const likeIds = await fetchCurrentUserLikeIds();
+
+  // console.log(members, likeIds);
 
   return (
-    <div className="mt-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-6 gap-8">
+    <div className="mt-10 grid grid-cols-2 md:grid-cols-3 xl:grid-cols-6 gap-8">
       {members &&
         members.map((member) => {
-          return <MemberCard key={member.id} member={member} />;
+          return (
+            <MemberCard key={member.id} member={member} likeIds={likeIds} />
+          );
         })}
     </div>
   );
