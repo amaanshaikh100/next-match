0ec9f7325251e44730fbd9f9150ea47d936ee0ee
Amaan Shaikh <shaikhamaannn100@gmail.com>
2025-06-08 15:26:19 +0530

    added list tabs to the lists page

--- src/app/lists/page.tsx
+++ src/app/lists/page.tsx
@@ -1,3 +1,22 @@
-export default function ListsPage() {
-  return <div>Lists</div>;
+import {
+  fetchCurrentUserLikeIds,
+  fetchLikedMembers,
+} from "../actions/likeActions";
+import ListsTab from "./ListsTab";
+
+export default async function ListsPage({
+  searchParams,
+}: {
+  searchParams: Promise<{ type: string }>;
+}) {
+  const { type } = await searchParams;
+
+  const likeIds = await fetchCurrentUserLikeIds();
+  const members = await fetchLikedMembers(type);
+
+  return (
+    <div>
+      <ListsTab members={members} likeIds={likeIds} />
+    </div>
+  );
 }
