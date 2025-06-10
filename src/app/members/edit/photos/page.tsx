c8bad5d6a4aff8956a7fce0b6bb58adc8996256c
Amaan Shaikh <shaikhamaannn100@gmail.com>
2025-06-10 18:31:56 +0530

    created the start and delete button component for photos

---
+++ src/app/members/edit/photos/page.tsx
@@ -0,0 +1,44 @@
+import { getAuthUserId } from "@/app/actions/authActions";
+import { getMemberPhotosByUserId } from "@/app/actions/memberActions";
+import DeleteButton from "@/components/DeleteButton";
+import StarButton from "@/components/StarButton";
+import { CardBody, CardHeader } from "@heroui/card";
+import Image from "next/image";
+
+export default async function PhotosPage() {
+  const userId = await getAuthUserId();
+
+  const photos = await getMemberPhotosByUserId(userId);
+
+  return (
+    <>
+      <CardHeader className="text-2xl font-semibold text-secondary">
+        Edit Photo
+      </CardHeader>
+
+      <CardBody>
+        <div className="grid grid-cols-5 gap-3 p-5">
+          {photos &&
+            photos.map((photo) => (
+              <div key={photo.id} className="relative">
+                <Image
+                  width={220}
+                  height={220}
+                  src={photo.url}
+                  alt="Image of user"
+                />
+
+                <div className="absolute top-3 left-3 z-50">
+                  <StarButton selected={false} loading={false} />
+                </div>
+
+                <div className="absolute top-3 right-3 z-50">
+                  <DeleteButton loading={false} />
+                </div>
+              </div>
+            ))}
+        </div>
+      </CardBody>
+    </>
+  );
+}
