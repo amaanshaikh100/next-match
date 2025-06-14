e0824954bb643b8de1ffb55dbdf738b28ed4c588
Amaan Shaikh <shaikhamaannn100@gmail.com>
2025-06-14 18:28:04 +0530

    added cloudinary for images

--- src/app/members/edit/photos/page.tsx
+++ src/app/members/edit/photos/page.tsx
@@ -1,12 +1,15 @@
 import { getAuthUserId } from "@/app/actions/authActions";
-import { getMemberPhotosByUserId } from "@/app/actions/memberActions";
-import DeleteButton from "@/components/DeleteButton";
-import StarButton from "@/components/StarButton";
+import {
+  getMemberByUserId,
+  getMemberPhotosByUserId,
+} from "@/app/actions/memberActions";
 import { CardBody, CardHeader } from "@heroui/card";
-import Image from "next/image";
+import MemberPhotoUpload from "./MemberPhotoUpload";
+import MemberPhotos from "@/components/MemberPhotos";
 
 export default async function PhotosPage() {
   const userId = await getAuthUserId();
+  const member = await getMemberByUserId(userId);
 
   const photos = await getMemberPhotosByUserId(userId);
 
@@ -17,27 +20,12 @@ export default async function PhotosPage() {
       </CardHeader>
 
       <CardBody>
-        <div className="grid grid-cols-5 gap-3 p-5">
-          {photos &&
-            photos.map((photo) => (
-              <div key={photo.id} className="relative">
-                <Image
-                  width={220}
-                  height={220}
-                  src={photo.url}
-                  alt="Image of user"
-                />
-
-                <div className="absolute top-3 left-3 z-50">
-                  <StarButton selected={false} loading={false} />
-                </div>
-
-                <div className="absolute top-3 right-3 z-50">
-                  <DeleteButton loading={false} />
-                </div>
-              </div>
-            ))}
-        </div>
+        <MemberPhotoUpload />
+        <MemberPhotos
+          photos={photos}
+          editing={true}
+          mainImageUrl={member?.image}
+        />
       </CardBody>
     </>
   );
